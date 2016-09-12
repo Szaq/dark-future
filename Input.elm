be19dcde905bde46237dee274c068a000ce940e7
Lukasz Kwoska <lukasz@kwoska.pl>
2016-09-12 22:32:49 +0200

    Submit input on enter

--- Input.elm
+++ Input.elm
@@ -1,7 +1,8 @@
 module Input exposing(Model, Msg(..), OutMsg(..), update, view)
 
 import Html exposing(input, button, text, div, Html)
-import Html.Events exposing(onInput, onClick)
+import Html.Events exposing(onInput, onClick, on)
+import Json.Decode as Json
 
 
 type alias Model = String
@@ -14,8 +15,16 @@ update msg model = case msg of
   ChangeText text -> (text, Nothing)
   SubmitClicked -> ("", Submit model)
 
+is13 : Int -> Result String ()
+is13 code =
+  if code == 13 then Ok () else Err "not the right key code"
+
+handleKeyPress : Json.Decoder Msg
+handleKeyPress =
+  Json.map (always SubmitClicked) (Json.customDecoder Html.Events.keyCode is13)
+
 view: Model -> Html Msg
 view model = div []
-              [input [onInput ChangeText][]
+              [input [onInput ChangeText, on "keypress" handleKeyPress][]
               ,button [onClick SubmitClicked][text "Submit"]
               ]
