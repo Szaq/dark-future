af63faf6edbe6d41c46dbbcf129078d07b412c1b
Lukasz Kwoska <lukasz@kwoska.pl>
2016-09-12 23:26:32 +0200

    Multiline output

--- History/Entry.elm
+++ History/Entry.elm
@@ -2,6 +2,7 @@ module History.Entry exposing(EntryType(..), Model, Msg, view, information)
 
 import Html exposing(..)
 import Time
+import String exposing(..)
 
 type EntryType = Speech | Information | Event
 
@@ -10,7 +11,10 @@ type alias Model = {date: Time.Time, text: String, entryType: EntryType}
 type Msg = None
 
 view: Model -> Html Msg
-view model = div [] [text model.text]
+view model = let displayLine line =  [div [] [text line]]
+                 displayLines lines = List.map displayLine lines |> List.concat
+                 lines = split "\n" model.text
+             in div [] (displayLines lines)
 
 information: String -> Model
 information text = Model 0 text Information
