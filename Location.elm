c2de9b73c53c5c1454150ce2f0686aedc95b6c4b
Lukasz Kwoska <lukasz@kwoska.pl>
2016-09-14 12:08:01 +0200

    Fixed movement not working

--- Location.elm
+++ Location.elm
@@ -10,48 +10,68 @@ type alias Id =
     Int
 
 
+type alias Locations =
+    Dict.Dict Int Model
+
+
 type alias Model =
     { name : String
     , description : String
     , exits : DirectionMap Id
     , items : List Item.Model
-    , characters : List Character.Model 
+    , characters : List Character.Model
     }
 
-locationsWithMovedCharacter: Character.Id -> Id -> Id -> Dict.Dict Int Model -> Maybe (Dict.Dict Int Model)
-locationsWithMovedCharacter characterId oldLocationId newLocationId locations = 
-            let oldLocation = 
-                    Dict.get oldLocationId locations
 
-                newLocation = 
-                    Dict.get newLocationId locations
+locationWithAddedCharacter : Character.Model -> Model -> Model
+locationWithAddedCharacter character location =
+    { location | characters = location.characters ++ [ character ] }
 
-                player = 
-                    oldLocation `Maybe.andThen` characterInLocation characterId
 
-                selectCharacter character =
-                    character.id == characterId 
+locationWithRemovedCharacter : Character.Model -> Model -> Model
+locationWithRemovedCharacter character location =
+    let
+        notEqual evaluatedCharacter =
+            character.id /= evaluatedCharacter.id
+    in
+        { location | characters = List.filter notEqual location.characters }
 
-                updatedOldLocation = 
-                    Maybe.map (\location -> 
-                                {location 
-                                    | characters = List.filter selectCharacter location.characters}) oldLocation
 
-                updatedNewLocation =
-                    Maybe.map2 (\location player ->
-                                             Just {location 
-                                                     | characters =  location.characters ++ [player]}) newLocation player
-                    |> Maybe.withDefault Nothing
-                     
+locationsWithMovedCharacter : Character.Id -> Id -> Id -> Locations -> Maybe (Locations)
+locationsWithMovedCharacter characterId oldLocationId newLocationId locations =
+    let
+        oldLocation =
+            Dict.get oldLocationId locations
 
-                in Maybe.map2 
-                    (\new old -> Dict.insert newLocationId new locations 
-                                |>  Dict.insert oldLocationId old) updatedNewLocation updatedOldLocation
-            
-selectCharacter: Character.Id -> Character.Model -> Maybe Character.Model
-selectCharacter id character =  if character.id == id then
-                                    Just character
-                                    else 
-                                    Nothing
-characterInLocation: Character.Id -> Model -> Maybe Character.Model
-characterInLocation id location = Maybe.oneOf <| List.map (selectCharacter id) location.characters
+        newLocation =
+            Dict.get newLocationId locations
+
+        player =
+            oldLocation `Maybe.andThen` characterInLocation characterId
+
+        updatedOldLocation =
+            Maybe.map2 locationWithRemovedCharacter player oldLocation
+
+        updatedNewLocation =
+            Maybe.map2 locationWithAddedCharacter player newLocation
+    in
+        Maybe.map2
+            (\new old ->
+                Dict.insert newLocationId new locations
+                    |> Dict.insert oldLocationId old
+            )
+            updatedNewLocation
+            updatedOldLocation
+
+
+selectCharacter : Character.Id -> Character.Model -> Maybe Character.Model
+selectCharacter id character =
+    if character.id == id then
+        Just character
+    else
+        Nothing
+
+
+characterInLocation : Character.Id -> Model -> Maybe Character.Model
+characterInLocation id location =
+    Maybe.oneOf <| List.map (selectCharacter id) location.characters
